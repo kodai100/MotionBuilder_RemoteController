4fa309344e8a996dbf4951915c83c11fd2140cc4
Kodai Takao <kodai100@gmail.com>
2019-11-26 17:09:20 +0900

    Change server initialization message

--- Util/UdpServer.py
+++ Util/UdpServer.py
@@ -6,7 +6,7 @@ class UdpServer():
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((host, port))
-        print port
+        print "Listening port : " + str(port)
         
     def close(self):
         self.sock.close()
