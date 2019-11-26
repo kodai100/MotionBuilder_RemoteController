851a6e1f4f6103a557848f0a2d6db3b1c8ccfa03
Kodai Takao <kodai100@gmail.com>
2019-11-26 16:25:51 +0900

    json文字列で制御変更できるようにして完成。Recordが有効になったデバイスがない状態Recしようとするとハングするので注意

--- RemoteController.py
+++ RemoteController.py
@@ -3,42 +3,89 @@ import os
 sys.path.append("D:\\work\\MotionBuilderDev\\RemoteController")
 
 from PySide import QtGui, QtCore
+from PySide import shiboken
+
 from pyfbsdk import *
 from pyfbsdk_additions import *
+
 from Util.StoppableThread import *
 from Util.UdpServer import *
 
-class WaitDialog(QtGui.QDialog):
+import json
+import traceback
+
+class NativeWidgetHolder(FBWidgetHolder):
+    def WidgetCreate(self, pWidgetParent):
+        
+        self.mNativeQtWidget = WaitDialog()
+        
+        return shiboken.getCppPointer(self.mNativeQtWidget)[0]
+
+class NativeQtWidgetTool(FBTool):
+    def BuildLayout(self):
+        x = FBAddRegionParam(0,FBAttachType.kFBAttachLeft,"")
+        y = FBAddRegionParam(0,FBAttachType.kFBAttachTop,"")
+        w = FBAddRegionParam(0,FBAttachType.kFBAttachRight,"")
+        h = FBAddRegionParam(0,FBAttachType.kFBAttachBottom,"")
+        self.AddRegion("main","main", x, y, w, h)
+        self.SetControl("main", self.mNativeWidgetHolder)
+         
+    def __init__(self, name):
+        FBTool.__init__(self, name)
+        self.mNativeWidgetHolder = NativeWidgetHolder()
+        self.BuildLayout()
+        self.StartSizeX = 200
+        self.StartSizeY = 200
+
+class WaitDialog(QtGui.QWidget):
 
     def WaitSignal(self):
+
         while True:
             
             data, addr = self.server.sock.recvfrom(1024) # buffer size is 1024 bytes
-            newTake = FBTake("S1_C24_Take1")
             
-            FBSystem().Scene.Takes.append(newTake)
-            FBSystem().CurrentTake=newTake
+            try:
+                decodedSignal = json.loads(str(data))
+                
+                if decodedSignal["signal"] == "player":
+                    if decodedSignal["value"] == "record":
+                        
+                        if self.playerControl.Record(True, False):
+                            self.play("Recording !")
+                            
+                    elif decodedSignal["value"] == "play":
+                        self.play()
+
+                    elif decodedSignal["value"] == "stop":
+                        self.stopButtonClicked()
+
+                elif decodedSignal["signal"] == "ready":
+                    newTake = FBTake(decodedSignal["value"].encode())
+                    FBSystem().Scene.Takes.append(newTake)
+                    FBSystem().CurrentTake=newTake
+
+            except Exception as e:
+                t, v, tb = sys.exc_info()
+                print traceback.format_exception(t,v,tb)
+                
+                continue
             
-            if self.playerControl.Record(True, False):
-                
-                self.playerControl.GotoStart()
-                self.playerControl.Play()
-                self.label.setText("Playing !")
-                self.label.setStyleSheet("QLabel{color:red;}")
-                
-            return
 
     def terminate(self):
         self.thread.stop()
         self.server.close()
-        self.close()
 
     def stopButtonClicked(self):
         self.playerControl.Stop()
-        self.terminate()
+        self.label = QtGui.QLabel("Ready ...")
+        self.label.setStyleSheet("QLabel{color:cyan;}")
 
-    def cancelButtonClicked(self):
-        self.terminate()
+    def play(self, label="Playing !"):
+        self.playerControl.GotoStart()
+        self.playerControl.Play()
+        self.label.setText(label)
+        self.label.setStyleSheet("QLabel{color:red;}")
 
     def closeEvent(self, event):
         self.terminate()
@@ -54,25 +101,42 @@ class WaitDialog(QtGui.QDialog):
 
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.label)
+
         stopButton = QtGui.QPushButton("Stop")
         stopButton.clicked.connect(self.stopButtonClicked)
-        cancelButton = QtGui.QPushButton("Cancel")
-        cancelButton.clicked.connect(self.cancelButtonClicked)
+
         horizon = QtGui.QHBoxLayout()
         horizon.addWidget(stopButton)
-        horizon.addWidget(cancelButton)
+
         layout.addLayout(horizon)
 
         self.setLayout(layout)
         # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowTitle("UDP Start Trigger")
-        self.resize(200, 100)
-        self.setModal(True)
-        self.show()
-        self.raise_()
 
         QtGui.QApplication.processEvents()
         self.thread = StoppableThread(target=self.WaitSignal)
         self.thread.start()
+
+
+gToolName = "Remote Controller"
+
+gDEVELOPMENT = True
+
+if gDEVELOPMENT:
+    FBDestroyToolByName(gToolName)
+
+if gToolName in FBToolList:
+    tool = FBToolList[gToolName]
+    ShowTool(tool)
+else:
+    tool=NativeQtWidgetTool(gToolName)
+    
+    FBAddTool(tool)
+    
+    if gDEVELOPMENT:
+        ShowTool(tool)
+
+
 
 app = WaitDialog()
